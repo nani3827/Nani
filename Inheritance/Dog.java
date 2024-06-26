97aa0c4a81651e2d5b268a13f5aac830045c74ac
Fibbinocci <tanakalanaveen2016@gmail.com>
2024-06-26 17:42:43 +0530

    Inheritance Addeed successfully

---
+++ Inheritance/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+    void makesound(){
+        System.out.println("Dod barks bow bow");
+
+    }
+
+    public static void main(String[] args) {
+        Dog kuby=new Dog();
+        kuby.makesound();
+        kuby.makesound();
+    }
+}
